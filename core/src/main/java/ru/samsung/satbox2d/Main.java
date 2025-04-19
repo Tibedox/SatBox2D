d788bc29ff042032d10ae275288a751dbe969aeb
Tibedox <tibedox@mail.ru>
2025-04-19 16:34:40 +0300

    StaticBody floor

--- core/src/main/java/ru/samsung/satbox2d/Main.java
+++ core/src/main/java/ru/samsung/satbox2d/Main.java
@@ -6,26 +6,39 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
     public static final float WORLD_WIDTH = 16, WORLD_HEIGHT = 9;
     private SpriteBatch batch;
     private OrthographicCamera camera;
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, WORLD_WIDTH, WORLD_HEIGHT);
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+        debugRenderer = new Box2DDebugRenderer();
+
+        StaticBody floor = new StaticBody(world, 8, 1, 15.5f, 0.5f);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        debugRenderer.render(world, camera.combined);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.end();
+        world.step(1/60f, 6, 2);
     }
 
     @Override
